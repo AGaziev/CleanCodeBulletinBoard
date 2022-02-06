e02a572f834312a9cb9ce9987904880504fc0314
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-02-06 23:03:10 +0300

    Setup PHPStorm on my project

--- code/index.php
+++ code/index.php
@@ -1,2 +1,8 @@
 <?php
- echo phpinfo();
+echo "1.Доступ по ссылке\n";
+/* Imagine a lot of code here */
+$very_bad_unclear_name="15 chicken wings";
+// Write your code here:
+
+//// Don't change the line below
+echo"\nYour order is:$very_bad_unclear_name.";
