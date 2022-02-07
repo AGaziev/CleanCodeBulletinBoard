cca5db04025ffa5b128175fa84eb284722ab1c99
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-02-07 14:31:09 +0300

    replace \n with <br>

--- code/index.php
+++ code/index.php
@@ -1,20 +1,20 @@
 <?php
-echo "1.Доступ по ссылке\n";
+echo "1.Доступ по ссылке";
 /* Imagine a lot of code here */
 $very_bad_unclear_name="15 chicken wings";
 // Write your code here:
 $order = &$very_bad_unclear_name;
 $order .= " qwe";
 //// Don't change the line below
-echo"\nYour order is:$very_bad_unclear_name.";
-echo"\n 2.Числа\n";
+echo"<br>Your order is:$very_bad_unclear_name.";
+echo"<br> 2.Числа<br>";
 $var1 = 2;
-echo $var1 . "\n";
+echo $var1 . "<br>";
 $var2 = 323;
 echo $var2;
 $var3 = 2.323;
 echo " " . $var3 . " ";
-echo (5+7) . "\n";
+echo (5+7) . "<br>";
 $last_month = 1187.23;
 $this_month = 1089.98;
 echo ($last_month - $this_month);
