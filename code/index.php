50b82bafcd57a610bc51a569eabc0e603fc97666
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-02-07 14:38:32 +0300

    PhpLab1 add 11 point

--- code/index.php
+++ code/index.php
@@ -1,4 +1,5 @@
 <?php
+
 echo "1.Доступ по ссылке";
 /* Imagine a lot of code here */
 $very_bad_unclear_name="15 chicken wings";
@@ -7,6 +8,7 @@ $order = &$very_bad_unclear_name;
 $order .= " qwe";
 //// Don't change the line below
 echo"<br>Your order is:$very_bad_unclear_name.";
+
 echo"<br> 2.Числа<br>";
 $var1 = 2;
 echo $var1 . "<br>";
@@ -18,3 +20,11 @@ echo (5+7) . "<br>";
 $last_month = 1187.23;
 $this_month = 1089.98;
 echo ($last_month - $this_month);
+
+echo "<br> 11.Умножение и деление";
+$num_languages = 4;
+$months=11;
+$days = $months*16;
+$days_per_language=$days/$num_languages;
+echo "<br>" . $days_per_language;
+
