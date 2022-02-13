dc74cd52cf62e608158d444db9c883cae3710cd5
Alan Gaziev <agaziev@stud.kantiana.ru>
2022-02-13 23:59:07 +0300

    LAB3 form with 4 inputs and saving it all to one session !!array!!

--- code/index.php
+++ code/index.php
@@ -34,26 +34,26 @@ echo $result . '<br>';
 
 echo "2.Форма. Сессии и Куки<br>";
 ?>
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="US-ASCII">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Регулярные выражение и работа с файлами</title>
-</head>
-<body>
-<form method="POST">
-    <label>
+    <!doctype html>
+    <html lang="ru">
+    <head>
+        <meta charset="US-ASCII">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Регулярные выражение и работа с файлами</title>
+    </head>
+    <body>
+    <form method="POST">
+        <label>
 <textarea name="textToParse"
           placeholder="Введите текст в котором необходимо узнать количество слов и символов"
           rows="5" cols="40"></textarea><br>
-    </label>
-    <input type="submit" name="getWordsAndSyms" value="Обработать текст">
-</form>
-</body>
-</html>
+        </label>
+        <input type="submit" name="getWordsAndSyms" value="Обработать текст">
+    </form>
+    </body>
+    </html>
 <?php
 if ($_POST['getWordsAndSyms']) {
     if ($_POST['textToParse']) {
@@ -71,51 +71,22 @@ echo $_SESSION['textInfo'];
 Запишите эти данныев сессию.
 При заходе на другую страницу выведите этиданные на экран.*/
 ?>
-<body>
-<form method="POST">
-    <label>
-        NAME<input type="text" name="name" required><br>
-        SURNAME<input type="text" name="surname" required><br>
-        AGE<input type="number" name="age" required><br>
-        <input type="submit" value="Записать в сессию" name="sendToSession"><br>
-    </label>
-</form>
-</body>
+    <body>
+    <form method="POST">
+        <label>
+            NAME<input type="text" name="name" required><br>
+            SURNAME<input type="text" name="surname" required><br>
+            AGE<input type="number" name="age" required><br>
+            <input type="submit" value="Записать в сессию" name="sendToSession">
+        </label>
+    </form>
+    </body>
 <?php
 if ($_POST['sendToSession']) {
     if ($_POST['name'] && $_POST['surname'] && $_POST['age']) {
         $_SESSION['name'] = $_POST['name'];
         $_SESSION['surname'] = $_POST['surname'];
         $_SESSION['age'] = $_POST['age'];
-    }
-}
-?>
-<body>
-<form method="POST">
-    <label>
-        NAME<input type="text" name="nameV2" required><br>
-        SURNAME<input type="text" name="surnameV2" required><br>
-        SALARY<input type="number" name="salaryV2" required><br>
-        MARITAL STATUS
-        <select name="status" required>
-            <option>SINGLE</option>
-            <option>MARRIED</option>
-            <option>DIVORCED</option>
-            <option>WIDOW(ER)</option>
-        </select><br>
-        <input type="submit" value="Записать в сессию" name="sendToSessionArray"><br>
-    </label>
-    <a href="UserInfo.php">К информации пользователя</a>
-</form>
-</body>
-<?php
-if ($_POST['sendToSessionArray']) {
-    if ($_POST['nameV2'] && $_POST['surnameV2'] && $_POST['salaryV2'] && $_POST['status']) {
-        $_SESSION['userInfo'] = array('name' => $_POST['nameV2'],
-            'surname' => $_POST['surnameV2'],
-            'salary' => $_POST['salaryV2'],
-            'status' => $_POST['status']
-        );
     }
 }
 ?>
